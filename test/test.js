c6768c0793730fb72624a9477a450600439e3b9d
Nico <nicovigil1@gmail.com>
2019-03-23 14:43:58 -0600

    adds tests for updating a song by id

--- test/test.js
+++ test/test.js
@@ -1,6 +1,14 @@
 var app = require('../server')
 const test = require('supertest')
 const chai = require('chai');
+const knexCleaner = require('knex-cleaner')
+
+const environment = "development";
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration)
+database('favorites').where(database.raw("favorites.fav_id = ?", [1]))
+  .then(song => {song1 = song})
+
 // const should = chai.should();
 // const chaiHttp = require('chai-http');
 // const server = require('../server');
@@ -27,10 +35,21 @@ describe('favorites', () => {
         chai.assert(res.body.length, 1)
       })
   });
+  it('can update elements of a selected song', () => {
+    return test(app)
+      .put('/api/v1/favorites/1/?title=silvertron')
+      .expect(200)
+      .expect('{"success":"song successfully updated"}')
+  });
   it('can post to add a favorite', () => {
     return test(app)
       .post('/api/v1/favorites/queen/rock/90')
       .expect(200)
       .expect('{"success":"favorite added!"}')
-  })
+      .then(() => {
+        knexCleaner.clean(database).then(() => {})
+        database.seed.run()
+      }
+  )
+  });
 });
