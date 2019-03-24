ef3b1121e40b5c2a00ae1f8bf59d25044cfdf84b
Nico <nicovigil1@gmail.com>
2019-03-24 12:12:40 -0600

    minor fix to seeds file to get circle ci tests passing

--- seeds/favorites.js
+++ seeds/favorites.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('favorites').insert([
-        {id: 1, title: 'things1', artist: 'things1', rating: 5, fav_id: 1},
-        {id: 2, title: 'things2', artist: 'things2', rating: 5, fav_id: 2},
-        {id: 3, title: 'things3', artist: 'things3', rating: 5}
+        {title: 'things1', artist: 'things1', rating: 5, fav_id: 1},
+        {title: 'things2', artist: 'things2', rating: 5, fav_id: 2},
+        {title: 'things3', artist: 'things3', rating: 5}
       ]);
     });
 };
--- seeds/favorites_test__seed.js
+++ seeds/favorites_test__seed.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('favorites_test').insert([
-        { id: 4, title: 'things1', artist: 'things1', rating: 5, fav_id: 1 },
-        { id: 5, title: 'things2', artist: 'things2', rating: 5, fav_id: 1 },
-        { id: 6, title: 'things3', artist: 'things3', rating: 5 }
+        { title: 'things1', artist: 'things1', rating: 5, fav_id: 1 },
+        { title: 'things2', artist: 'things2', rating: 5, fav_id: 1 },
+        { title: 'things3', artist: 'things3', rating: 5 }
       ]);
     });
 };
