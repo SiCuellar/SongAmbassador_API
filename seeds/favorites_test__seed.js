02c0ad7e549a9b34a43f390f89b62a8c6e7be9b0
Nico <nicovigil1@gmail.com>
2019-03-24 10:53:07 -0600

    adds test database migrations and seeds

---
+++ seeds/favorites_test__seed.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('favorites_test').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('favorites_test').insert([
+        { id: 1, title: 'things1', artist: 'things1', rating: 5, fav_id: 1 },
+        { id: 2, title: 'things2', artist: 'things2', rating: 5 },
+        { id: 3, title: 'things3', artist: 'things3', rating: 5 }
+      ]);
+    });
+};
