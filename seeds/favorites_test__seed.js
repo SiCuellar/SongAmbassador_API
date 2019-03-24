a11f474cf4595188ae760409b4821637b5bf6fdf
Nico <nicovigil1@gmail.com>
2019-03-24 12:00:20 -0600

    minor fix to seeds file to get circle ci tests passing

--- seeds/favorites_test__seed.js
+++ seeds/favorites_test__seed.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('favorites_test').insert([
         { id: 1, title: 'things1', artist: 'things1', rating: 5, fav_id: 1 },
-        { id: 2, title: 'things2', artist: 'things2', rating: 5 },
+        { id: 2, title: 'things2', artist: 'things2', rating: 5, fav_id: 1 },
         { id: 3, title: 'things3', artist: 'things3', rating: 5 }
       ]);
     });
